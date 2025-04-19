11accc33287c8648628017f7b65ec2a0134458da
Tanya Dixit <shanya.2018td@gmail.com>
2025-04-19 17:30:08 +0530

    Updated oauth_handler.py file

--- slack_bot_app/handlers/oauth_handler.py
+++ slack_bot_app/handlers/oauth_handler.py
@@ -1,23 +1,22 @@
 import requests
 from slack_bolt.app import App
 from slack_bolt.oauth.callback_options import CallbackOptions
+from slack_bolt.response import BoltResponse
 
-def handle_oauth_success(request, response):
+def handle_oauth_success(request):
     installation = request.context.get("installation")
     install_data = installation.to_dict()
     try:
         requests.post("https://jadepalace.onrender.com/slack/store-auth", json=install_data)
         print("✅ Install data sent successfully.")
+        return BoltResponse(status=200, body="OAuth installation successful.")
     except Exception as e:
         print(f"❌ Error sending install data: {e}")
-    return response
+        return BoltResponse(status=500, body="Failed to store installation data.")
 
-def handle_oauth_failure(request, response):
+def handle_oauth_failure(request):
     print(f"Request: {request}")
-    print(f"Response: {response}")
-    response.status = 500
-    response.body = "OAuth installation failed."
-    return response
+    return BoltResponse(status=500, body="OAuth installation failed.")
 
 def setup_oauth_handlers(app: App):
     app.oauth_flow.callback_options = CallbackOptions(
